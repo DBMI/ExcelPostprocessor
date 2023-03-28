30e1a2c7e2e48d49aaa23e01e2bbfd1e9b90dc26
kevin-dbmi <kjdelaney@ucsd.edu>
2023-03-28 12:41:37 -0700

    Refactor parser_runner.py to break into several functions.

--- src/excelpostprocessor/__main__.py
+++ src/excelpostprocessor/__main__.py
@@ -13,41 +13,43 @@ def main() -> None:
     #   Handle 'help' case.
     parser = argparse.ArgumentParser(
         description=r"""
-        Excel Postprocessor applies Regular Expressions to an existing Excel workbook, extracting data 
-        into a new column. You must provide an XML configuration file that lists: 
-        
+        Excel Postprocessor applies Regular Expressions to an existing Excel workbook, extracting data
+        into a new column. You must provide an XML configuration file that lists:
+
         1) Name of the Excel workbook to process
-        2) Sheet(s) to be processed 
+        2) Sheet(s) to be processed
         3) The Regular Expression to use and the new column to be created. Here's an example file:
-        
+
         <workbook>
             <name>test_data.xlsx</name>
             <sheet>
-                <name>Patients</name> 
+                <name>Patients</name>
                 <source_column>
                     <name>Report</name>
                     <extract>
-                        <pattern>Date of Exam:\s?(\d{1,2}/\d{1,2}/\d{4})</pattern> 
+                        <pattern>Date of Exam:\s?(\d{1,2}/\d{1,2}/\d{4})</pattern>
                         <new_column>Date of Exam</new_column>
                     </extract>
                     <extract>
-                        <pattern>LV EF MOD BP:\s?(\d+\.?\d*\s?)%</pattern> 
+                        <pattern>LV EF MOD BP:\s?(\d+\.?\d*\s?)%</pattern>
                         <new_column>LV EF %</new_column>
                     </extract>
                 </source_column>
             </sheet>
         </workbook>
-        
+
         Run the app with:
             excel_postprocess.exe --config <name of config file.xml>
-            
-        If the config file name is not specified, app will look for the default file excel_postprocessor.xml. 
+
+        If the config file name is not specified, app will look for the default file excel_postprocessor.xml.
         """,
         epilog="""The app creates a new Excel workbook for each worksheet to be processed.""",
-        formatter_class=argparse.RawTextHelpFormatter
+        formatter_class=argparse.RawTextHelpFormatter,
     )
 
-    parser.add_argument('--config', default=CONFIG_FILENAME, help='Name of XML config file.')
+    parser.add_argument(
+        "--config", default=CONFIG_FILENAME, help="Name of XML config file."
+    )
     args = parser.parse_args()
 
     if os.path.exists(args.config):
@@ -55,8 +57,6 @@ def main() -> None:
         runner.process()
     else:
         parser.print_help()
-
-
 
 
 if __name__ == "__main__":
